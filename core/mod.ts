65a3109666aea940ca3e04c154e849f4ca55e11b
Cole <schultzcole@gmail.com>
2024-11-02 16:49:37 -0500

    re-export reactive stuff from mod.ts

---
+++ core/mod.ts
@@ -0,0 +1,4 @@
+export { reactive } from "./src/reactive/reactive-proxy.ts"
+export type { Reactiveable, ReactiveProxy } from "./src/reactive/reactive-proxy.ts"
+export { default as ReactiveContext } from "./src/reactive/reactive-context.ts"
+export type { EffectOptions, TriggerMode } from "./src/reactive/reactive-context.ts"
