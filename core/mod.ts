43057ee3a309bf4f5ec22e9bd836dc370578576c
Cole <schultzcole@gmail.com>
2024-11-05 23:08:15 -0600

    export ReactiveHtmlBuilder from core

--- core/mod.ts
+++ core/mod.ts
@@ -3,4 +3,5 @@ export type { Reactiveable, ReactiveProxy } from "./src/reactive/reactive-proxy.
 export { default as ReactiveContext } from "./src/reactive/reactive-context.ts"
 export type { EffectOptions, TriggerMode } from "./src/reactive/reactive-context.ts"
 export { default as HtmlBuilder } from "./src/html/html-builder.ts"
+export { default as ReactiveHtmlBuilder } from "./src/reactive-html/reactive-html-builder.ts"
 export * from "./src/html/html-types.ts"
