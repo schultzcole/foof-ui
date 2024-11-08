4e54932ca36ff7990ab1bbb4446e00591c9ba0e9
Cole <schultzcole@gmail.com>
2024-11-07 21:19:03 -0600

    improve styling of demo and add 7guis

--- examples/basic-example/src/counter.ts
+++ examples/basic-example/src/counter.ts
@@ -4,20 +4,18 @@ export function counter(root: ReactiveHtmlBuilder, initialValue: number) {
     const { state, ctx } = reactive({ count: initialValue })
 
     root.tag("div", (div) => {
-        div.attrs({ className: "flex-row-spaced" })
-            .style({ fontSize: "2rem" })
+        div.attrs({ className: "flex flex-row flex-gap flex-even" })
+            .style({ fontSize: "1.4rem" })
             .tag("button", (button) => {
-                button.attrs({ className: "small-drop-shadow" })
+                button
                     .text("-")
                     .on("mousedown", (_) => state.count--)
             })
             .tag("span", (span) => {
-                span.reactive(ctx, (template) => {
-                    template.html`Count: <strong>${state.count}</strong>`
-                })
+                span.reactive(ctx, (template) => template.html`Count: <strong>${state.count}</strong>`)
             })
             .tag("button", (button) => {
-                button.attrs({ className: "small-drop-shadow" })
+                button
                     .text("+")
                     .on("mousedown", (_) => state.count++)
             })
--- examples/basic-example/src/main.ts
+++ examples/basic-example/src/main.ts
@@ -1,16 +1,56 @@
+import { ReactiveHtmlBuilder } from "@scope/core"
 import "./style.css"
 import { counter } from "./counter.ts"
-import { ReactiveHtmlBuilder } from "@scope/core"
+import { temperatureConverter } from "./temperature-converter.ts"
 
 const rootElement = document.getElementById("app")!
 new ReactiveHtmlBuilder("template")
-    .tag("h1", (heading) => heading.text("Foof Demo"))
-    .tag("div", (card) => {
-        card.attrs({ className: "flex-col" })
-            .tag("div", (panel) => {
-                panel.attrs({ className: "panel drop-shadow" })
-                    .tag("h2", (h2) => h2.text("Counter"))
-                    .component(counter, 42)
-            })
+    .tag("h1", (heading) => heading.text("FOOF 7GUIs"))
+    .tag("div", (column) => {
+        column.attrs({ className: "flex flex-col flex-gap" })
+
+        column.tag("div", (panel) => {
+            panel.attrs({ className: "panel drop-shadow" })
+                .html(
+                    `<p>A demo of Eugen Kiss's <a href="https://eugenkiss.github.io/7guis" target="_blank">7GUIs</a> UI framework benchmark implemented in FOOF.</p>`,
+                )
+        })
+
+        column.tag("div", (panel) => {
+            panel.attrs({ className: "panel drop-shadow" })
+                .html(`<h2>Counter</h2>`)
+                .component(counter, 42)
+        })
+
+        column.tag("div", (panel) => {
+            panel.attrs({ className: "panel drop-shadow" })
+                .html(`<h2>Temperature Converter</h2>`)
+                .component(temperatureConverter)
+        })
+
+        column.tag("div", (panel) => {
+            panel.attrs({ className: "panel drop-shadow" })
+                .html(`<h2>Flight Booker</h2>`)
+        })
+
+        column.tag("div", (panel) => {
+            panel.attrs({ className: "panel drop-shadow" })
+                .html(`<h2>Timer</h2>`)
+        })
+
+        column.tag("div", (panel) => {
+            panel.attrs({ className: "panel drop-shadow" })
+                .html(`<h2>Address Book</h2>`)
+        })
+
+        column.tag("div", (panel) => {
+            panel.attrs({ className: "panel drop-shadow" })
+                .html(`<h2>Circle Drawer</h2>`)
+        })
+
+        column.tag("div", (panel) => {
+            panel.attrs({ className: "panel drop-shadow" })
+                .html(`<h2>Cells</h2>`)
+        })
     })
     .mount(rootElement)
